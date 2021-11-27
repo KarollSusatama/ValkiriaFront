84c5c4ca8c82600d04a7e1de45016b9d5ef0ecb4
GonzaCamargo <68657750+GonzaCamargo@users.noreply.github.com>
2021-11-27 15:43:33 -0500

    cambios nuevos

--- valkiria_app/src/components/Headers/AuthHeader.js
+++ valkiria_app/src/components/Headers/AuthHeader.js
@@ -31,15 +31,14 @@ const AuthHeader = () => {
               color={theme.palette.white.main}
             >
               <Grid item lg={5} md={6} xs={12}>
-                <h1>Welcome!</h1>
+                <h1>Bienvenida a tu lugar seguro!</h1>
                 <Box
                   component="p"
                   color={theme.palette.gray[400]}
                   lineHeight="1.7"
                   fontSize="1rem"
                 >
-                  Use these awesome forms to login or create new account in your
-                  project for free.
+                  Ingresa tus datos personales para registrarte en nuestra plataforma.
                 </Box>
               </Grid>
             </Box>
